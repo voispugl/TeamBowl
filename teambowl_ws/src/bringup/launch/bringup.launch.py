87e354d14bae971d78e6fde6af1b2334cfc27cde
Eesha Palasamudrum <eeshapalasamudrum@Eeshas-MacBook-Pro-7.local>
2026-03-21 17:53:15 -0400

    trying camera point cloud processing

--- teambowl_ws/src/bringup/launch/bringup.launch.py
+++ teambowl_ws/src/bringup/launch/bringup.launch.py
@@ -20,6 +20,7 @@ def generate_launch_description():
         launch_arguments={
             'name': 'oak',
             'rectify_rgb': 'true',
+            'pointcloud.enable': 'false',
             'params_file': os.path.join(
                 get_package_share_directory('depthai_ros_driver'),
                 'config', 'oak_d_pro_w.yaml'
