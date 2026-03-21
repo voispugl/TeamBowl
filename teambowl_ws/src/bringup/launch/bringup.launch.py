93c0821086c87bbeb9fa3b928ff7844072c8d33b
Eesha Palasamudrum <eeshapalasamudrum@Eeshas-MacBook-Pro-7.local>
2026-03-21 17:30:03 -0400

    fix again

--- teambowl_ws/src/bringup/launch/bringup.launch.py
+++ teambowl_ws/src/bringup/launch/bringup.launch.py
@@ -19,7 +19,7 @@ def generate_launch_description():
         ),
         launch_arguments={
             'name': 'oak',
-            'rectify_rgb': 'false',
+            'rectify_rgb': 'true',
             'params_file': os.path.join(
                 get_package_share_directory('depthai_ros_driver'),
                 'config', 'oak_d_pro_w.yaml'
