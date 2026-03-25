0f8fbbed70f0a1539e65f2349c409735ba081b8f
Eesha Palasamudrum <eeshapalasamudrum@Eeshas-MacBook-Pro-7.local>
2026-03-24 22:50:07 -0400

    .

--- teambowl_ws/source.sh
+++ teambowl_ws/source.sh
@@ -11,6 +11,7 @@ sudo ip link set can0 type can bitrate 1000000
 sudo ip link set can1 type can bitrate 1000000
 sudo ip link set can0 up
 sudo ip link set can1 up
+sudo ip link set can0 txqueuelen 1000
 
 echo "[build] Sourcing ROS Humble..."
 source /opt/ros/humble/setup.bash
